9a119b1e7dbaadf06814f35b3921b61b6fc49e3b
Kanataason <tatsu.uzaimikan@gmail.com>
2026-03-07 15:38:13 +0900

    script

--- Assets/BaseScript/CharaBase.cs
+++ Assets/BaseScript/CharaBase.cs
@@ -7,15 +7,20 @@ public class CharaBase : MonoBehaviour
     public virtual void SetState() { }//画面切り替え時点何をしているのかhp,flagや（アニメーション）を保存
                               //アニメーションが現在何秒まで進んでいるのかを
     public virtual void GetState() {}//前回のステータスをセット
-    public virtual void TakeDamage(int damage) { }//攻撃を食らったときの関数
+    public virtual void TakeDamage(int damage) { m_hp -= damage; }//攻撃を食らったときの関数
     public virtual void Die() { }//死んだとき
 
+    protected Animator a_Animator;
     protected int m_hp;
     protected SaveState c_SaveState = new SaveState();
 }
 [System.Serializable]
 public class SaveState
 {
+    //アニメの処理
+    public float m_AnimeTime;
+    public int m_AnimeHash;
+
     public int m_Inihp;
     public Vector3 v_IniPosition;
     public Quaternion q_IniRotate;
--- Assets/TaTsuya/Script/BossMoveManager.cs
+++ Assets/TaTsuya/Script/BossMoveManager.cs
@@ -5,17 +5,39 @@ public class BossMoveManager : CharaBase
     
     private void Start()
     {
-        
+        a_Animator = GetComponent<Animator>();
+        m_hp = 100;
     }
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
+        if(Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * 0.5f*Time.deltaTime);
+            a_Animator.SetFloat("Move", 3f * Time.deltaTime, 0.05f, Time.deltaTime);
+            transform.Translate(Vector3.left * 3f*Time.deltaTime);
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * 0.5f * Time.deltaTime);
+            a_Animator.SetFloat("Move",0, 0.05f, Time.deltaTime);
+            transform.Translate(Vector3.right * 4f * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.H)) { TakeDamage(5); }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            c_SaveState.m_AnimeTime = a_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+            c_SaveState.m_AnimeHash = a_Animator.GetCurrentAnimatorStateInfo(0).fullPathHash;
+            c_SaveState.m_Inihp = m_hp;
+            c_SaveState.v_IniPosition = transform.position;
+            c_SaveState.q_IniRotate = transform.rotation;
+            Debug.Log($"hp{c_SaveState.m_Inihp}/pos{c_SaveState.v_IniPosition}/rotete" +
+                $"{c_SaveState.q_IniRotate}/animetime{c_SaveState.m_AnimeTime}");
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            a_Animator.Play(c_SaveState.m_AnimeHash, 0, c_SaveState.m_AnimeTime);
+            m_hp = c_SaveState.m_Inihp;
+            transform.position = c_SaveState.v_IniPosition;
+            transform.rotation = c_SaveState.q_IniRotate;
+            Debug.Log($"hp{m_hp}/pos{transform.position}/rotete{transform.rotation}");
         }
     }
     
