b2a2ecccbe58a956264ae4fbfdfa85c5a951d914
smoker0009 <24171006@oic-ok.ac.jp>
2026-03-18 13:06:32 +0900

    ビルドするための設定

--- Assets/BaseScript/CharaBase.cs
+++ Assets/BaseScript/CharaBase.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Overlays;
+//using UnityEditor.Overlays;
 using UnityEngine;
 using UnityEngine.U2D;
 using UnityEngine.UI;
